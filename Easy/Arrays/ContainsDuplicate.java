05bcfebf66b991586d1711f692fb3265e7e65ada
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-19 23:11:02 +0530

    217. Contains Duplicate

---
+++ Easy/Arrays/ContainsDuplicate.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+//217. Contains Duplicate
+public class ContainsDuplicate {
+    public static boolean containsDuplicate(int[] nums){
+        Arrays.sort(nums);
+        int n = nums.length;
+        // for(int i=0; i<n-1; i++){
+        //     if(nums[i] == nums[i+1]){
+        //         return true;
+        //     }
+        // }
+        for(int i=1; i<n; i++){
+            if(nums[i] == nums[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int[] nums = new int[m];
+        for(int i=0; i<m; i++){
+            nums[i] = sc.nextInt();
+        } 
+        System.out.println(containsDuplicate(nums));
+    }
+}
