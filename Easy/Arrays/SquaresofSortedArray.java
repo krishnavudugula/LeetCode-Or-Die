bc63fee703e884caf1470cc06cc3c83d5740d1b6
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-20 15:30:48 +0530

    977. Squares of Sorted Array

--- Easy/Arrays/SquaresofSortedArray.java
+++ Easy/Arrays/SquaresofSortedArray.java
@@ -1,20 +1,39 @@
 //977. Squares of a Sorted Array
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 public class SquaresofSortedArray {
     public static int[] squareofArray(int[] nums){
-        int result[] = new int[nums.length];
-        for(int i=0;i<nums.length; i++){
-            result[i] = nums[i] * nums[i];
+        // int result[] = new int[nums.length];
+        // for(int i=0;i<nums.length; i++){ //TC : n
+        //     result[i] = nums[i] * nums[i];
+        // }
+        // Arrays.sort(result); //nlogn
+        // return result;
+
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+        for(int i=n-1; i>=0; i--){
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if(leftSquare > rightSquare){
+                result[i] = leftSquare;
+                left++;
+            } else{
+                result[i] = rightSquare;
+                right--;
+            }
         }
-        Arrays.sort(result);
         return result;
+
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] nums = new int[n];
-        for(int i=0; i<n; i++){
+        int m = sc.nextInt();
+        int[] nums = new int[m];
+        for(int i=0; i<m; i++){
             nums[i] = sc.nextInt();
         }
         System.out.println(Arrays.toString(squareofArray(nums)));
