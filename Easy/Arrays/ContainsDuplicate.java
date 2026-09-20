bbdb93709b1ef04cd58e8458c7ebb9997db74585
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-20 08:44:54 +0530

    217. Contains Duplicate - Set Approach

--- Easy/Arrays/ContainsDuplicate.java
+++ Easy/Arrays/ContainsDuplicate.java
@@ -1,21 +1,32 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 //217. Contains Duplicate
 public class ContainsDuplicate {
     public static boolean containsDuplicate(int[] nums){
-        Arrays.sort(nums);
-        int n = nums.length;
+        // Arrays.sort(nums);
+        // int n = nums.length;
         // for(int i=0; i<n-1; i++){
         //     if(nums[i] == nums[i+1]){
         //         return true;
         //     }
         // }
-        for(int i=1; i<n; i++){
-            if(nums[i] == nums[i-1]){
+
+
+        // for(int i=1; i<n; i++){
+        //     if(nums[i] == nums[i-1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        Set <Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(set.contains(num)){
                 return true;
             }
+            set.add(num);
         }
         return false;
+
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
