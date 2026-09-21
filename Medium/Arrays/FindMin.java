6023ffd215ff755919e1887680b895745b5b05d1
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-21 07:28:55 +0530

    153. Find Min in Rotated SA - O(logn)

--- Medium/Arrays/FindMin.java
+++ Medium/Arrays/FindMin.java
@@ -2,14 +2,29 @@
 import java.util.*;
 public class FindMin {
     public static int findMin(int[] nums){
-        int min = nums[0];
+        // int min = nums[0];
 
-        for(int i =1; i<nums.length; i++){
-            if(nums[i] < min){
-                min = nums[i];
+        // for(int i =1; i<nums.length; i++){
+        //     if(nums[i] < min){
+        //         min = nums[i];
+        //     }
+        // }    
+        // return min;       //----------> But O(n) TC
+        int m = nums.length;
+        int left = 0;
+        int right = m - 1;
+
+        while(left < right){
+            int mid = ( left + right ) / 2; //But java int can only hold up to : 2,147,483,647.
+            if(nums[mid] > nums[right]) {
+                left = mid + 1;
             }
-        }    
-        return min;       //----------> But O(n) TC
+            else {
+                right = mid - 1;
+            }
+        }
+        return nums[left];
+
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
