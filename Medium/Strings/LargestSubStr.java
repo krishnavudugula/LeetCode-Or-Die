291a49177c263c27628d98fab0f99edc9da64390
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-21 19:03:05 +0530

    3. Longest Substring without Repeating Characters

---
+++ Medium/Strings/LargestSubStr.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class LargestSubStr {
+   public static void main(String[] args) {
+      String str = "abcabcbb";
+      System.out.println(str);
+      char[] c = str.toCharArray();
+      int i = 0;
+      int max = 0;
+      Set <Character> c1 = new HashSet<>();
+      for(int j=0; j<c.length; j++){
+         while(c1.contains(c[j])){
+            c1.remove(c[i]);
+            i++;
+         }
+         c1.add(c[j]);
+         max = Math.max(max, j - i + 1);
+      }
+      System.out.println(max);
+   }
+}
