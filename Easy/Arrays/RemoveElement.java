d95e92bcb3063e639b783c25d5ec1db1a1a65c69
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-22 22:32:07 +0530

    27. Remove Element

---
+++ Easy/Arrays/RemoveElement.java
@@ -0,0 +1,17 @@
+public class RemoveElement {
+    public static int removeElement(int[] nums, int val){
+        int k = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,4,2,5};
+        int val = 2;
+        System.out.println(removeElement(nums, val));
+    }
+}
