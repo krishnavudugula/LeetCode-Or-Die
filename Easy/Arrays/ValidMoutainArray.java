7332854f2cb9e08d0ab467aba8de5553344683c2
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-22 23:23:08 +0530

    941. Valid Mountain Array

---
+++ Easy/Arrays/ValidMoutainArray.java
@@ -0,0 +1,22 @@
+public class ValidMoutainArray {
+    public static boolean validMountainArray(int[] arr){
+        if(arr.length < 3){
+            return false;
+        }
+        int i = 0;
+
+        //Mountain Going UP
+        while(i < arr.length - 1 && arr[i] < arr[i+1]){
+            i++;
+        }
+
+        if(i==0 || i == arr.length - 1){
+            return false;
+        }
+        //Coming Down
+        while(i< arr.length - 1 && arr[i] > arr[i+1]){
+            i++;
+        }
+        return i == arr.length - 1;   
+    }
+}
