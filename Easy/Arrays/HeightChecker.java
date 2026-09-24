a96f021a79cf665e7ec8b1bd47578dd26e899161
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-24 07:53:36 +0530

    1051. Height Checker

---
+++ Easy/Arrays/HeightChecker.java
@@ -0,0 +1,19 @@
+//1051. Height Checker
+import java.util.*;
+public class HeightChecker {
+    public static int heightChecker(int[] heights){
+        int[] expected = new int[heights.length];
+        for(int i=0; i<heights.length; i++){
+            expected[i] = heights[i];
+        }
+        Arrays.sort(expected);
+
+        int count = 0;
+        for(int i=0; i<heights.length; i++){
+            if(heights[i] != expected[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
