aa180893baf25ce350d0b21b24b09cf30143c2b8
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-25 07:28:41 +0530

    414. Third Max Array - UserDefined

---
+++ Easy/Arrays/ThirdMaxArray.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class ThirdMaxArray {
+    public static int thirdMaxArray(int[] nums){
+        long first = Long.MIN_VALUE;
+        long second = Long.MIN_VALUE;
+        long third = Long.MIN_VALUE;
+
+        for(int num : nums){
+            if(num == first || num == second || num == third){
+                continue;
+            }
+            if(num > first){
+                third = second;
+                second = first;
+                first = num;
+            }
+            else if(num > second){
+                third = second;
+                second = num;
+            }
+            else if(num > third){
+                third = num;
+            }
+        }
+        //If there is no third Distinction value.
+        if(third == Long.MIN_VALUE){
+            return (int) first;
+        }
+        return (int) third;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for(int i =0; i<n; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println(thirdMaxArray(nums));
+    }
+}
