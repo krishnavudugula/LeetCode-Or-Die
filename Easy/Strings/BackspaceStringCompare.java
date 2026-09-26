96286041645e55da8c7b90520d95405384cd15aa
Krishna Vudugula <krishnavudugula2005@gmail.com>
2026-09-26 12:31:00 +0530

    844. Backspace String Compare

---
+++ Easy/Strings/BackspaceStringCompare.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class BackspaceStringCompare {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> sStack = new Stack<>();
+        Stack<Character> tStack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '#') {
+                if (!sStack.isEmpty()) {
+                    sStack.pop();
+                }
+            } else {
+                sStack.push(ch);
+            }
+        }
+        for (char ch : t.toCharArray()) {
+            if (ch == '#') {
+                if (!tStack.isEmpty()) {
+                    tStack.pop();
+                }
+            } else {
+                tStack.push(ch);
+            }
+        }
+        return sStack.equals(tStack);
+    }
+}
